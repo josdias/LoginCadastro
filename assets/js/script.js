43240ae9cb9578564a9d7b4fc6737060b49bb6da
José Dias <devjosedias@gmail.com>
2023-04-13 18:30:03 -0300

    Criação do modo mobile

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,13 @@
 const presentationLogin = document.querySelector(".presentation-login");
 const presentationRegister = document.querySelector(".presentation-register");
 const formLogin = document.querySelector(".form-login");
-const fomrRegister = document.querySelector(".form-register")
+const formRegister = document.querySelector(".form-register");
 const login = document.querySelector(".login");
 const register = document.querySelector(".register");
-setTimeout(remover, 2000);
 
 function openRegister() {
   presentationRegister.classList.remove("animation-right");
-  fomrRegister.classList.remove("animation-left");
+  formRegister.classList.remove("animation-left");
   presentationLogin.classList.add("animation-left");
   formLogin.classList.add("animation-right");
   setTimeout(function () {
@@ -23,7 +22,35 @@ function openLogin() {
   presentationLogin.classList.remove("animation-left");
   formLogin.classList.remove("animation-right");
   presentationRegister.classList.add("animation-right");
-  fomrRegister.classList.add("animation-left");
+  formRegister.classList.add("animation-left");
+  setTimeout(function () {
+    login.classList.remove("remove");
+    login.classList.add("add");
+    register.classList.add("remove");
+    register.classList.remove("add");
+  }, 900);
+}
+
+/* mobile */
+
+function openRegisterMobile() {
+  formLogin.classList.remove("animation-down-top");/* remove down top */
+  formRegister.classList.remove("animation-top"); /* remove top */
+  formLogin.classList.add("animation-down"); /* add down */
+  formRegister.classList.add("animation-top-down"); /* add top down */
+  setTimeout(function () {
+    login.classList.add("remove");
+    login.classList.remove("add");
+    register.classList.add("add");
+    register.classList.remove("remove");
+  }, 900);
+}
+
+function openLoginMobile() {
+  formRegister.classList.remove("animation-top-down"); /* remove top down */
+  formLogin.classList.remove("animation-down"); /* remove down */
+  formRegister.classList.add("animation-top"); /* add top */
+  formLogin.classList.add("animation-down-top"); /* add down top */
   setTimeout(function () {
     login.classList.remove("remove");
     login.classList.add("add");
